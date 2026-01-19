8f0317f79a918c4f62026f7583f23892d89847bc
omodotori <aktaevaamina07@gmail.com>
2026-01-19 10:08:50 +0500

    Initial commit: Spring Boot Chat Server with WebSocket and Docker support

---
+++ src/main/java/com/example/chat/model/ChatMessage.java
@@ -0,0 +1,20 @@
+package com.example.chat.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+}
